b0e5b3689395c97be61022e23b6e0ccb180218a5
ashleynicolas <96598937+ashleynicolas@users.noreply.github.com>
2025-04-15 22:10:15 -0500

    Added comments and pylint suggestions
    Used pylint to get feedback and made code corrections. Added comments for clarity.

--- score_headlines.py
+++ score_headlines.py
@@ -1,30 +1,31 @@
+# This python script takes in text files and uses and SVM model to assign sentiment scores for headlines.
 import sys
-from sentence_transformers import SentenceTransformer
-import joblib
 import datetime
-
-# score_headlines.py
+import joblib
+from sentence_transformers import SentenceTransformer
 
 if len(sys.argv) < 3:
-    print("ERROR: Missing parameter. Please include text file name and source name: 'python score_headlines.py <TEXT_FILE> <SOURCE_NAME>'")
+    print("ERROR: Missing parameter. Please include text file name and source name.")
+    print("FORMAT: python score_headlines.py <TEXT_FILE> <SOURCE_NAME>")
     print("EXAMPLE: python score_headlines.py nyt_headlines_today.txt nyt")
     sys.exit(1)
 
 TEXT_FILE = sys.argv[1]
 SOURCE_NAME = sys.argv[2]
 
-with open(TEXT_FILE, 'r') as file:
+# Read in text file to create headline vectors for model classification
+with open(TEXT_FILE, 'r', encoding="utf-8") as file:
     headlines = file.readlines()
-    
 model = SentenceTransformer("all-MiniLM-L6-v2")
 embeddings = model.encode(headlines)
 
+# Load in trained SVM model to classify headline based on vector embeddings
 clf = joblib.load('svm.joblib')
-
 scores = clf.predict(embeddings)
 
 TODAY = datetime.datetime.today().strftime('%Y_%m_%d')
 
+# Write output file with scores followed by corresponding headline
 OUTPUT_FILE = f"headline_scores_{SOURCE_NAME}_{TODAY}.txt"
 with open(OUTPUT_FILE, 'w', encoding='utf-8') as output:
     for score, headline in zip(scores, headlines):
