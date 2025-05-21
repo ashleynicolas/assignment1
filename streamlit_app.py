9743440e4c8d03181dc7923438af7d2799c1942c
ashleynicolas <96598937+ashleynicolas@users.noreply.github.com>
2025-05-21 17:44:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,31 +6,37 @@ st.title("Headline Sentiment Analyzer")
 
 if "headlines" not in st.session_state:
     st.session_state.headlines=[""]
+if "scores" not in st.session_state:
+     st.session_state["scores"] = []
 
 def add_headline():
-    st.session_state.headlines.append("")
+    st.session_state["headlines"].append("")
 
 def delete_headline():
-    if len(st.session_state.headlines) > 1:
-        st.session_state.headlines.pop()
+    if len(st.session_state["headlines"]) > 1:
+        st.session_state["headlines"].pop()
 
-for idx in range(len(st.session_state.headlines)):
+def score_headlines()
+        headlines = [headline for headline in st.session_state.headlines]
+        response = requests.post(url = "http://127.0.0.1:8082/score_headlines", json=headlines)
+        scores = response.json().get("Sentiment Scores",[])
+        st.session_state["scores"] = list(zip(headlines, scores))
+
+for idx in range(len(st.session_state["headlines"])):
     st.session_state.headlines[idx] = st.text_input("Please enter your headline here", 
-                                                    value = st.session_state.headlines[idx],
+                                                    value = st.session_state["headlines"][idx],
                                                     key = f"Headlines_{idx+1}")
 
 col1, col2 = st.columns(2)
-
 with col1:
-    headline_input = st.text_input("Please enter headline below:")
     st.button("Add a headline", on_click = add_headline)
 
 with col2:
     st.button("Delete a headline",on_click=delete_headline)
 
-if st.button("Score Headlines"):
-        headlines = [headline for headline in st.session_state.headlines]
-        response = requests.post(url = "http://127.0.0.1:8082/score_headlines", json=headlines)
-        scores = response.json().get("Sentiment Scores",[])
-        for headline, score in zip(headlines, scores):
-             st.markdown(f"##{headline}: *{score}*")
+st.button("Score Headlines", on_click=score_headlines)
+
+if st.session_state["scores"]:
+     st.markdown("#Scores:")
+     for headline,score in st.session_state["scores"]:
+          st.markdown(f"###{headline}: **{score}")
