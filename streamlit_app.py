3418f9c6b2b8931712cebfd791ba933ced15b592
ashleynicolas <96598937+ashleynicolas@users.noreply.github.com>
2025-05-21 17:24:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,22 +4,19 @@ import streamlit as st
 
 st.title("Headline Sentiment Analyzer")
 
-def headlines_list():
-    if "headlines" not in st.session_state:
-        st.session_state.headlines=[""]
-        st.session_state.next_key = 1
+if "headlines" not in st.session_state:
+    st.session_state.headlines=[""]
 
 def add_headline():
     st.session_state.headlines.append("")
-    st.session_state.next_key += 1
-    st.experimental_rerun()
 
-def delete_headline(idx):
+def delete_headline():
     if len(st.session_state.headlines) > 1:
-        st.session_state.headlines.pop(idx)
-        st.experimental_rerun()
-        
-headlines_list()
+        st.session_state.headlines.pop()
+
+st.session_state.headlines[idx] = st.text_input("Please enter your headline here", 
+                                                value = st.session_state.headlines[idx],
+                                                key = f"Headlines_{idx+1}")
 
 col1, col2 = st.columns(2)
 
@@ -30,11 +27,9 @@ with col1:
 with col2:
     st.button("Delete a headline",on_click=delete_headline)
 
-def score_headlines():
-    response = requests.post(url = "http://127.0.0.1:8082/score_headlines", json=st.session_state.headlines)
-    scores = response.json().get()
-    st.markdown(scores)
-
-if st.button("Score Headlines", on_click=score_headlines):
-    pass
-
+if st.button("Score Headlines"):
+        headlines = [headline for headline in st.session_state.headlines]
+        response = requests.post(url = "http://127.0.0.1:8082/score_headlines", json=headlines)
+        scores = response.json().get("Sentiment Scores",[])
+        for headline, score in zip(headlines, scores):
+             st.markdown(f"##{headline}: *{score}*")
