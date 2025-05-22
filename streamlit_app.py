fae2f0e0ccc73b9223ee20b44b9cd422c4561a28
ashleynicolas <96598937+ashleynicolas@users.noreply.github.com>
2025-05-21 20:40:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import streamlit as st
 
 st.title("Headline Sentiment Analyzer")
 
+# Check of \\\\
 if "headlines" not in st.session_state:
     st.session_state["headlines"]=[""]
 if "scores" not in st.session_state:
@@ -16,21 +17,10 @@ def delete_headline():
     if len(st.session_state["headlines"]) > 1:
         st.session_state["headlines"].pop()
 
-def score_headlines():
-        response = requests.post(url = "http://127.0.0.1:8082/score_headlines", json=headlines)
-        response.raise_for_status()
-        data = response.json()
-        scores = data.get("scores", [])
-        st.session_state["scores"] = list(zip(headlines, scores))
-
-headlines=[]
 for idx in range(len(st.session_state["headlines"])):
-    val = st.text_input("Please enter your headline here:",
-                        value = st.session_state["headlines"][idx],
-                        key = f"Headlines_{idx+1}")
-    st.session_state["headlines"][idx] = val
-    if val.strip():
-         headlines.append(val)
+    st.session_state.headlines = st.text_input("Please enter your headline here:",
+                                               value = st.session_state["headlines"][idx],
+                                               key = f"Headlines_{idx}")
 
 col1, col2 = st.columns(2)
 with col1:
@@ -39,9 +29,13 @@ with col1:
 with col2:
     st.button("Delete a headline",on_click=delete_headline)
 
-st.button("Score Headlines", on_click=score_headlines)
+if st.button("Score Headlines"):
+    response = requests.post(url = "http://127.0.0.1:8082/score_headlines", json=st.session_state.headlines)
+    response.raise_for_status()
+    data = response.json()
+    st.session_state.scores = data.get("labels", [])
 
 if st.session_state["scores"]:
      st.markdown("#Scores:")
-     for headline,score in st.session_state["scores"]:
+     for headline,score in st.session_state.headlines, st.session_state.scores:
           st.markdown(f"###{headline}: **{score}")
