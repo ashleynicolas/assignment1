6511202a943b1c263d905c2aca7b9441a7d87574
ashleynicolas <96598937+ashleynicolas@users.noreply.github.com>
2025-05-21 21:06:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import streamlit as st
 
 st.title("Headline Sentiment Analyzer")
 
-# Check of \\\\
 if "headlines" not in st.session_state:
     st.session_state["headlines"]=[""]
 if "scores" not in st.session_state:
@@ -17,25 +16,34 @@ def delete_headline():
     if len(st.session_state["headlines"]) > 1:
         st.session_state["headlines"].pop()
 
-for idx in range(len(st.session_state["headlines"])):
-    st.session_state.headlines = st.text_input("Please enter your headline here:",
-                                               value = st.session_state["headlines"][idx],
-                                               key = f"Headlines_{idx}")
+for i in range(len(st.session_state["headlines"])):
+    st.session_state.headlines[i] = st.text_input("Please enter your headline here:",
+                                               value = st.session_state["headlines"][i],
+                                               key = f"Headline_{i}")
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 with col1:
     st.button("Add a headline", on_click = add_headline)
 
 with col2:
     st.button("Delete a headline",on_click=delete_headline)
 
-if st.button("Score Headlines"):
-    response = requests.post(url = "http://127.0.0.1:8082/score_headlines", json=st.session_state.headlines)
-    response.raise_for_status()
-    data = response.json()
-    st.session_state.scores = data.get("labels", [])
+with col3:
+    if st.button("Score Headlines"):
+        headline_list = [i for i in st.session_state.headline if h.strip()]
+        if not headline_list:
+                st.warning("Please add a headline before scoring.")
+        else:
+            try:
+                response = requests.post(url = "http://127.0.0.1:8082/score_headlines", json=st.session_state.headlines)
+                response.raise_for_status()
+                data = response.json()
+                st.session_state.scores = data.get("labels", [])
+            except requests.RequestException as e:
+                st.error(f"Error connecting to server: {e}")
+                st.session_state.scores = []
 
 if st.session_state["scores"]:
      st.markdown("#Scores:")
-     for headline,score in st.session_state.headlines, st.session_state.scores:
+     for headline,score in zip(headline_list, st.session_state.scores):
           st.markdown(f"###{headline}: **{score}")
